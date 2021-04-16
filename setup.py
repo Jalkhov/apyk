a63adc99ea3f33ceabbf619c81615aa32437eca5
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-04-16 07:59:56 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="APyK",
+    version="0.0.1",
+    description="APyK is a program with which you will be able to backup your applications from your computer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Pedro Torcatt",
+    author_email="pedrotorcattsoto@gmail.com",
+    url="https://github.com/Jalkhov/apyk",
+    license="MIT",
+    keywords="adb android apk backup apps",
+    packages=['apyk'],
+    install_requires=[
+        'PyQt5',
+        'google_play_scraper'
+    ],
+    entry_points={
+        'console_scripts': [
+            'apyk = apyk.WIN_Main:main',
+        ],
+    },
+    classifiers=[
+        "Topic :: Utilities",
+        "Environment :: Win32 (MS Windows)",
+        "Topic :: System :: Archiving :: Backup",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 4 - Beta",
+    ],
+)
